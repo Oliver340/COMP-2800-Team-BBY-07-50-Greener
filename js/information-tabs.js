8bcc33e41b23981b57190f33f21265f34063f61e
Kyung Min Song <ksong34@my.bcit.ca>
2021-05-06 00:08:58 -0700

    removed console logs

--- js/information-tabs.js
+++ js/information-tabs.js
@@ -4,22 +4,15 @@ var tablinks = tabmenu.children;
 var tabcontents = document.getElementById('tabcontents');
 
 
-
 $('#tabmenu').on('click', '.tablink', function (e) {
 
     let activetab = e.target.id.replace("-tab", "");
-    console.log(e.target.id);
-    console.log(activetab);
     showTab(e, activetab);
 
 });
 
 
-
 function showTab(event, tabID) {
-
-    console.log(event.target.className);
-    console.log(event.target.className);
 
     $('.tab-body').attr('style', 'display: none');
     $('.tablink-active').attr('class', 'tablink');
