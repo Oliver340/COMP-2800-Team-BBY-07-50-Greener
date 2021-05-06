0993c51a310d03ab977799db1782acc25b36a922
Kyung Min Song <ksong34@my.bcit.ca>
2021-05-06 16:20:00 -0700

    added function to close  active tabs, modified shadows on elements on information.html and challenges.html

--- js/tabs.js
+++ js/tabs.js
@@ -2,17 +2,23 @@ $(function () {
 
     $('.tab-body').toggle();
 
+    $('#tabmenu').on('click', '.tablink-active', function (e) {
+        let activetab = e.target.id.replace("-tab", "");
+        $(`#${activetab}`).toggle();
+    });
+
     $('#tabmenu').on('click', '.tablink', function (e) {
 
         let activetab = e.target.id.replace("-tab", "");
         showTab(e, activetab);
+
     });
 
-    function showTab(event, tabID) {
+    function showTab(e, tabID) {
         $('.tab-body').attr('style', 'display: none');
         $('.tablink-active').attr('class', 'tablink');
 
-        event.target.className += "-active";
+        e.target.className += "-active";
         $(`#${tabID}`).attr('style', 'display: block');
     }
 });
