fc365be73dce3f24571ac0475fb0253aaaa99c0b
Kyung Min Song <ksong34@my.bcit.ca>
2021-05-06 20:07:44 -0700

    added function to update icon on click for challenges.html, and a function for closing tabs, and a condition to close other tabs for information.html

--- js/tabs.js
+++ js/tabs.js
@@ -2,23 +2,43 @@ $(function () {
 
     $('.tab-body').toggle();
 
-    $('#tabmenu').on('click', '.tablink-active', function (e) {
-        let activetab = e.target.id.replace("-tab", "");
-        $(`#${activetab}`).toggle();
+    $('#tabmenu').on('click', '.tablink-active > span', function (e) {
+        udpateIcon(e);
+        let activetab = e.target.parentNode.id.replace("-tab", "");
+        closeTab(e, activetab);
     });
 
-    $('#tabmenu').on('click', '.tablink', function (e) {
-
-        let activetab = e.target.id.replace("-tab", "");
+    $('#tabmenu').on('click', '.tablink > span', function (e) {
+        udpateIcon(e);
+        let activetab = e.target.parentNode.id.replace("-tab", "");
         showTab(e, activetab);
-
     });
 
     function showTab(e, tabID) {
-        $('.tab-body').attr('style', 'display: none');
-        $('.tablink-active').attr('class', 'tablink');
-
-        e.target.className += "-active";
+        let bodyid = e.target.parentNode.parentNode.parentNode.id;
+        if (bodyid == 'informationTabs') {
+            $('.tablink-active').attr('class', 'tablink');
+            $('.tab-body').attr('style', 'display: none');
+        }
+        e.target.parentNode.className += "-active";
         $(`#${tabID}`).attr('style', 'display: block');
+    }
+
+    function closeTab(e, tabID) {
+        e.target.parentNode.className = "tablink";
+        $(`#${tabID}`).attr('style', 'display: none');
+    }
+
+    function udpateIcon(e) {
+        let iconName = e.target.innerHTML;
+        let cname = e.target.parentNode.className;
+
+        console.log(cname);
+        if (iconName == 'expand_less') {
+            e.target.innerHTML = 'expand_more';
+        }
+        if (iconName == 'expand_more') {
+            e.target.innerHTML = 'expand_less';
+        }
     }
 });
