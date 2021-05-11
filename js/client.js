683a82a585b1345eeb90eb5dfaf7ee293325df95
Avneet Sandhu <aksx97@gmail.com>
2021-05-11 18:17:41 -0400

    created client and server side code for serving up login content when login button is clicked from index

--- js/client.js
+++ js/client.js
@@ -1,24 +1,44 @@
 'use strict';
 
-$(document).ready(function () {
+$(function () {
 
-  $("#index-signup").click(function () {
+  $("#index-signup").on("click", function () {
     $.ajax({
       url: "/signup",
       type: "GET",
       dataType: "JSON",
       data: {},
-      success: function(data) {
-          if(data['status'] == "success") {
-              window.location.replace("/signup");
-          } else {
-              $("#error").html(data['msg']);
-          }
+      success: function (data) {
+        if (data['status'] == "success") {
+          window.location.replace("/signup");
+        } else {
+          $("#error").html(data['msg']);
+        }
 
       },
-      error: function(jqXHR, textStatus, errorThrown) {
-          $("body").text(jqXHR.statusText);
+      error: function (jqXHR, textStatus, errorThrown) {
+        $("body").text(jqXHR.statusText);
       }
+    });
   });
+
+  $("#index-login").on("click", function () {
+    $.ajax({
+      url: "/login",
+      type: "GET",
+      dataType: "JSON",
+      data: {},
+      success: function (data) {
+        if (data['status'] == "success") {
+          window.location.replace("/login");
+        } else {
+          $("#error").html(data['msg']);
+        }
+
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $("body").text(jqXHR.statusText);
+      }
+    });
   });
 });
