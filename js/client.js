d599406b177699dccebf5cb242f44c9e14ebdb7d
Avneet Sandhu <aksx97@gmail.com>
2021-05-11 19:01:10 -0400

    changes to client.js click functions for login and signup

--- js/client.js
+++ js/client.js
@@ -3,42 +3,10 @@
 $(function () {
 
   $("#index-signup").on("click", function () {
-    $.ajax({
-      url: "/signup",
-      type: "GET",
-      dataType: "JSON",
-      data: {},
-      success: function (data) {
-        if (data['status'] == "success") {
-          window.location.replace("/signup");
-        } else {
-          $("#error").html(data['msg']);
-        }
-
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $("body").text(jqXHR.statusText);
-      }
-    });
+    window.location.replace("/signup");
   });
 
   $("#index-login").on("click", function () {
-    $.ajax({
-      url: "/login",
-      type: "GET",
-      dataType: "JSON",
-      data: {},
-      success: function (data) {
-        if (data['status'] == "success") {
-          window.location.replace("/login");
-        } else {
-          $("#error").html(data['msg']);
-        }
-
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $("body").text(jqXHR.statusText);
-      }
-    });
+    window.location.replace("/login");
   });
 });
