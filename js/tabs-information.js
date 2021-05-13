2dd348c9f28c79c004a69447694128a3502202f0
Kyung Min Song <ksong34@my.bcit.ca>
2021-05-12 18:50:37 -0700

    adjusted information tabs css and js function, fixed favicon link and resized the logo

--- js/tabs-information.js
+++ js/tabs-information.js
@@ -6,7 +6,7 @@ $(function () {
 
     $('#info').attr('style', 'display: block');
 
-    $('#tabmenu').on('click', '.tablink', function (e) {
+    $('.tablink, .tablink-active').on('click', 'span', function (e) {
 
         activetab = e.target.parentNode.id.replace('-tab', '');
         showTab(e, activetab);
@@ -14,11 +14,13 @@ $(function () {
     });
 
     function showTab(e, tabID) {
+        if (e.target.id != "tabmenu") {
 
-        $('.tablink-active').attr('class', 'tablink');
-        $('.tab-body').attr('style', 'display: none');
-        e.target.parentNode.className = "tablink-active";
-        $(`#${tabID}`).attr('style', 'display: block');
+            $('.tablink-active').attr('class', 'tablink');
+            $('.tab-body').attr('style', 'display: none');
+            e.target.parentNode.className = "tablink-active";
+            $(`#${tabID}`).attr('style', 'display: block');
+        }
 
     }
 
